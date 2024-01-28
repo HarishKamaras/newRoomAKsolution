a9bf50699ba9db635d95713c0ed2868b03b05402
HarishKamaras <harishkamarasu@gmail.com>
2024-01-28 19:40:53 +0530

    first commit newsRoom

---
+++ NewsRoom.jsx
@@ -0,0 +1,133 @@
+import React from 'react'
+
+function NewsRoom() {
+  return (
+    <div>
+<div id="main-content">
+	<div className="pageContainer">
+		<div className="h-50 d-flex justify-content-center align-items-center object-fit-fill" style={{ backgroundColor : "red" , backgroundSize:"auto" , color:'white' , backgroundImage: "url(https://insemitech.com/wp-content/uploads/2022/04/newsroom-banner-bg.jpg)" }}>
+            
+			<div className="">
+				<div className="col-sm-9 col-center">
+					<div className="blogsHeaderContent">
+						<h1 className="font-white text-center">Newsroom</h1>
+						<h4 className="text-center font-white">Take a look at some recent InSemi references in the news</h4>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div className="d-flex h-100 justify-content-center align-items-center " style={{}}>
+			<div className="w-75 h-75 d-flex "  style={{}}>
+            {/* <div className="d-flex align-content-center  w-75 "  style={{backgroundColor:"pink"}}> */}
+				
+													<div className="row">
+					
+													<div className="col-sm-4">
+								<div className="newsItem shadow">
+									<div className="card">
+										<div className="pt-4 text-center">
+		                  <img style={{width: "auto!important"}} src="https://insemitech.com/wp-content/uploads/2024/01/Insemi.png" className="" alt=""/>
+		                </div>
+	                  <div className="card-body">
+	                  	<p className="card-category"><time datetime="2024-01-11T18:57:48+05:30" itemprop="datePublished">11/01/2024</time> </p>
+	                  	<p className="fw-bold">Infosys to Acquire Leading Semiconductor Design Services Provider, InSemi</p>
+	                    <p></p><p>Infosys&nbsp;(NSE, BSE, NYSE: INFY) a global leader in next-generation digital services and consulting, today announced a definitive agreement to acquire&nbsp;InSemi,[…]</p>
+<p></p>
+
+	                    <a style={{textDecoration:"none"}} href="https://www.infosys.com/newsroom/press-releases/2024/acquire-semiconductor-design-services.html" className="stretched-link " target="_blank" >Read More →</a>
+	                  </div>
+	                </div>
+	              </div>
+	            </div>
+
+					
+													<div className="col-sm-4">
+								<div className="newsItem shadow">
+									<div className="card">
+										<div className="pt-4 text-center">
+		                  <img style={{width: "auto!important"}} src="https://insemitech.com/wp-content/uploads/2022/11/142.png" className="" alt=""/>
+		                </div>
+	                  <div className="card-body">
+	                  	<p className="card-category"><time datetime="2022-11-24T19:04:12+05:30" itemprop="datePublished">24/11/2022</time> </p>
+	                  	<p className="fw-bold">InSemi &amp; Ausdia – Two Tech Leaders Partner for Semicon Design Excellence!</p>
+	                    <p></p><p>InSemi is&nbsp;on an expansion spree, dedicated to providing the most spectacular results. A key step in this regard is their[…]</p>
+<p></p>
+
+	                    <a style={{textDecoration:"none"}} href="https://insemitech.com/blogs/insemi-ausdia-partnership/" className="stretched-link" target="_blank">Read More →</a>
+	                  </div>
+	                </div>
+	              </div>
+	            </div>
+
+					
+													<div className="col-sm-4">
+								<div className="newsItem shadow">
+									<div className="card">
+										<div className="pt-4 text-center">
+		                  <img style={{width: "auto!important"}} src="https://insemitech.com/wp-content/uploads/2022/04/newsroom-the-week.jpg" className="" alt=""/>
+		                </div>
+	                  <div className="card-body">
+	                  	<p className="card-category"><time datetime="2020-05-07T06:01:51+05:30" itemprop="datePublished">07/05/2020</time> </p>
+	                  	<p className="fw-bold">InSemiTech Acquires Ambit Semiconductors and Semtosys</p>
+	                    <p></p><p>InSemi, a leading semiconductor and embedded technology design company, announces the acquisition of two leading engineering services companies. The sensational[…]</p>
+<p></p>
+
+	                    <a style={{textDecoration:"none"}} href="https://www.theweek.in/wire-updates/business/2020/05/07/pwr3-insemitech.html" className="stretched-link" target="_blank">Read More →</a>
+	                  </div>
+	                </div>
+	              </div>
+	            </div>
+
+					
+					<div className="col-sm-4">
+								<div classNameName=" newsItem shadow">
+									<div className="card">
+										<div className="pt-4 text-center">
+		                  <img style={{width: "auto!important"}} src="https://insemitech.com/wp-content/uploads/2022/04/newsroom-the-week.jpg" className="" alt=""/>
+		                </div>
+	                  <div className="card-body">
+	                  	<p className="card-category"><time datetime="2020-02-26T13:13:07+05:30" itemprop="datePublished">26/02/2020</time> </p>
+	                  	<p className="fw-bold">InSemi Tech Announces the Acquisition of GateLength Technology</p>
+	                    <p></p><p>With the collective goal of increasing revenues and focusing more on organic/inorganic growth strategies, InSemi Tech announces its acquisition of[…]</p>
+<p></p>
+
+	                    <a style={{textDecoration:"none"}} href="https://www.theweek.in/wire-updates/business/2020/02/26/pwr10-insemitech.html" className="stretched-link" target="_blank">Read More →</a>
+	                  </div>
+	                </div>
+	              </div>
+	            </div>
+
+					
+													<div className="col-sm-4">
+								<div className="newsItem shadow">
+									<div className="card">
+										<div className="pt-4 text-center">
+		                  <img style={{width: "auto!important"}} src="https://insemitech.com/wp-content/uploads/2022/04/newsroom-silicon-india.jpg" className="" alt=""/>
+		                </div>
+	                  <div className="card-body">
+	                  	<p className="card-category"><time datetime="2019-04-29T13:13:11+05:30" itemprop="datePublished">29/04/2019</time> </p>
+	                  	<p className="fw-bold">InSemi: IoT Practices to Bring Devices Together</p>
+	                    <p></p><p>Silicon India exquisitely presents the depiction of life at InSemi and its journey into becoming a leading semiconductor and embedded[…]</p>
+<p></p>
+
+	                    <a style={{textDecoration:"none"}} href="https://technology.siliconindia.com/vendor/insemi-iot-practices-to-bring-devices-together-cid-8782.html" className="stretched-link" target="_blank">Read More →</a>
+	                  </div>
+	                </div>
+	              </div>
+	            </div>
+
+									</div>
+							</div>
+		</div>
+
+	</div>
+</div>
+
+
+
+
+    </div>
+  )
+}
+
+export default NewsRoom
